841fa6f487ea06e3fde4a455d93e6de44eb6289a
Cláudia Alves <claudia.alves@rupeal.com>
2014-03-27 16:48:07 +0000

    create model RocketAction and relationship between RocketAction and Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,12 +4,13 @@ class Employee < ActiveRecord::Base
   validates :email, format: { allow_blank: true, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i , message: INVALID_EMAIL}
   validate :greater_or_equal_than_18_years_old
   validates :birth_date, presence:{ message: BIRTH_DATE_REQUIRED}
+  has_many :rocket_actions
+
   private
   def greater_or_equal_than_18_years_old
   	if self.birth_date != nil
   	  errors.add(:birth_date, MINIMUM_AGE_18) if self.birth_date > 18.years.ago
     end
   end
-
 end
 
