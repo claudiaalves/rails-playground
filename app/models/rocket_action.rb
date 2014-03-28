6dc993af8b5e57c0538c7bb7d2f869352673d5d2
Cláudia Alves <claudia.alves@rupeal.com>
2014-03-28 11:30:20 +0000

    new rocket action

--- app/models/rocket_action.rb
+++ app/models/rocket_action.rb
@@ -1,4 +1,6 @@
 class RocketAction < ActiveRecord::Base
+	include ErrorString
+	validates :name, presence: {message: NAME_REQUIRED}
 	validates :points, numericality: {greater_than_or_equal_to: 0, only_integer: true}
 	has_and_belongs_to_many :employees
 end
